8f74d567a2b1ae6336c7de5b44ca68a352ca5a08
AndrejLapin <lapin.andrej1@gmail.com>
2025-01-26 13:18:55 +0200

    ambient pipe sounds

--- Scripts/manager.gd
+++ Scripts/manager.gd
@@ -8,12 +8,23 @@ var player_instance: Player = null:
 		player_instance = value
 		player_set.emit()
 var gameplay_instance: Gameplay = null
+var ambient_sounds: Array[AmbientSound]
+var delay_from: float = 3.0
+var delay_up_to: float = 10.0
+
+var current_delay: float = 0.0
+var ambient_sound_delay_elapsed: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	current_delay = randf_range(delay_from, delay_up_to)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	ambient_sound_delay_elapsed += delta
+	if ambient_sound_delay_elapsed >= current_delay:
+		ambient_sound_delay_elapsed -= current_delay
+		current_delay = randf_range(delay_from, delay_up_to)
+		var sound_pos := randi_range(0, ambient_sounds.size() - 1)
+		ambient_sounds[sound_pos].play_random_sound()
