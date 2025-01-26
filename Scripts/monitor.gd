d09f165b547049b3179ce03418b6c5fddfcef6a5
AndrejLapin <lapin.andrej1@gmail.com>
2025-01-26 12:57:36 +0200

    bubble growing with sound

--- Scripts/monitor.gd
+++ Scripts/monitor.gd
@@ -6,6 +6,12 @@ extends Node3D
 @onready var stock_ui := $SubViewport/StockUI
 var stock_ref: StockSimulated
 
+var update_stock_graphics: Callable = _nothing
+
+
+func _nothing() -> void:
+	pass
+
 
 func set_stock_name(name: StringName) -> void:
 	stock_ui.set_stock_name(name)
@@ -13,10 +19,13 @@ func set_stock_name(name: StringName) -> void:
 
 func set_stock_ref(stock: StockSimulated) -> void:
 	stock_ref = stock
+	update_stock_graphics = _update_stock_render
+	stock_ref.stock_updated.connect(_update_stock_render)
 	stock_ref.stock_died.connect(_on_stock_died)
 
 
 func _on_stock_died() -> void:
+	update_stock_graphics = _nothing
 	stock_ref.stock_died.disconnect(_on_stock_died)
 	_play_turn_off_sound()
 	$SubViewport/TurnOff.turn_off()
@@ -37,12 +46,13 @@ func _ready():
 	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if stock_ref == null:
-		return
+func _update_stock_render() -> void:
 	stock_ui.set_stock_history(stock_ref.price_history)
 	stock_ui.set_stock_buy_price(stock_ref.buy_price)
 	stock_ui.set_stock_sell_price(stock_ref.sell_price)
 	stock_ui.set_units_owned(stock_ref.stock_owned)
 	stock_ui.set_evaluation(stock_ref.evaluation)
+
+
+func _process(_delta: float) -> void:
+	update_stock_graphics.call()
