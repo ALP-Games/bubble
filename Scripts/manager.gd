f98b3c2db796be991eb34d1281dc9a2e26b2f34d
AndrejLapin <lapin.andrej1@gmail.com>
2025-01-26 14:41:40 +0200

    final sounds

--- Scripts/manager.gd
+++ Scripts/manager.gd
@@ -15,13 +15,20 @@ var delay_up_to: float = 10.0
 var current_delay: float = 0.0
 var ambient_sound_delay_elapsed: float = 0.0
 
+var process_ambient_sounds: Callable = _process_ambient_sound
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	gameplay_instance.the_end.connect(func()->void:process_ambient_sounds=func(delta: float):pass)
 	current_delay = randf_range(delay_from, delay_up_to)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	process_ambient_sounds.call(delta)
+
+
+func _process_ambient_sound(delta: float) -> void:
 	ambient_sound_delay_elapsed += delta
 	if ambient_sound_delay_elapsed >= current_delay:
 		ambient_sound_delay_elapsed -= current_delay
